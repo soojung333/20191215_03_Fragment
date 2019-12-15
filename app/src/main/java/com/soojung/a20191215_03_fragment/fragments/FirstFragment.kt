91100c778eec5335ba9e7b03111ac9e03bf0013a
soojung <blessme333@gmail.com>
2019-12-15 15:41:44 +0900

    프래그먼트 배치   - 코틀린 / xml 생성   - 액티비티 위에 얹기

---
+++ app/src/main/java/com/soojung/a20191215_03_fragment/fragments/FirstFragment.kt
@@ -0,0 +1,20 @@
+package com.soojung.a20191215_03_fragment.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.soojung.a20191215_03_fragment.R
+
+class FirstFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+}
