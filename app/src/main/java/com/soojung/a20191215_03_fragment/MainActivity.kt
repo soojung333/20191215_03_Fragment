20773dbc7cc85cf5918ee2aae605fc2be9a899d7
soojung <blessme333@gmail.com>
2019-12-15 15:32:08 +0900

    Initial commit

---
+++ app/src/main/java/com/soojung/a20191215_03_fragment/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.soojung.a20191215_03_fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+}
