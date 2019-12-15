52563ac23bccda7fa72a6258fd9ecdbb8c8f985b
soojung <blessme333@gmail.com>
2019-12-15 16:09:44 +0900

    두번째 프래그먼트 간략하게 생성

--- app/src/main/java/com/soojung/a20191215_03_fragment/fragments/SecondFragment.kt
+++ app/src/main/java/com/soojung/a20191215_03_fragment/fragments/SecondFragment.kt
@@ -4,33 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.fragment.app.Fragment
 import com.soojung.a20191215_03_fragment.R
-import kotlinx.android.synthetic.main.fragment_first.*
 
-class FirstFragment : BaseFragment(){
+class SecondFragment :BaseFragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
         setupEvents()
         setValues()
+
+
 
     }
 
 
+
     override fun setupEvents() {
-        okBtn.setOnClickListener {
-            Toast.makeText(mContext, "${nameEdt.text.toString()} / ${nickNameEdt.text.toString()}", Toast.LENGTH_SHORT).show()
-        }
 
     }
 
@@ -39,7 +38,4 @@ class FirstFragment : BaseFragment(){
     }
 
 
-
 }
-
-
